e425352d747b50030e9794353f3630008e99405e
JunJie-GitHub <2388791993@qq.com>
2020-11-08 22:23:21 +0800

    增加视频背景

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function() {
-   console.log("这是还没写的js");
+
+   //兼容新版的Chrome不能自动播放视频(静音)
+   var video = document.getElementById('bgvid');
+   video.play()
+       .then(() => {
+          // 播放成功啦
+       }, () => {
+          //播放失败
+          video.muted = true; // 设置为静音然后再次播发
+          video.play();
+       })
 });
